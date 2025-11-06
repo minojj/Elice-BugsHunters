7752bd35f384357329c881e5e8dcf82108106fb6
minojj <wrnx8vzkyz@privaterelay.appleid.com>
2025-11-06 13:30:50 +0900

    create all

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
