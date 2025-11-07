4a03ca0110ee4fe04b3a78d1f06f7ba2f1c50de7
yejin1024 <yejin1024@gmail.com>
2025-11-07 15:21:35 +0900

    import 수정
    import pyautogui 추가

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pyautogui    #추가 import 파일업로드 창 입력 (pip install pyautogui)
 
 def wait_for(driver, by, locator, timeout=10):
     return WebDriverWait(driver, timeout).until(EC.presence_of_element_located((by, locator)))
