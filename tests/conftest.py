ddf26bde6161246cb24e3d28275d3b16eaef6d46
yejin1024 <yejin1024@gmail.com>
2025-11-10 11:19:10 +0900

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
 
 @pytest.fixture
 def driver():
@@ -16,3 +22,9 @@ def driver():
     
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def wait(driver):
+    """공용 WebDriverWait 객체"""
+    return WebDriverWait(driver, 10)
