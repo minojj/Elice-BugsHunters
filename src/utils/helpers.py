95039034f1bf960352bd2860407ba22e70095012
minojj <wrnx8vzkyz@privaterelay.appleid.com>
2025-11-12 11:31:51 +0900

    edit helpers

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -5,7 +5,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import pyautogui    #추가 import 파일업로드 창 입력 (pip install pyautogui)
+import os
 
+if os.environ.get("PYAUTOGUI_OFF") != "1":
+    import pyautogui
+else:
+    pyautogui = None
 
 class Utils:
     def __init__(self, driver: WebDriver):
