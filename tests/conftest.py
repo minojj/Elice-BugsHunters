bcbdde5d8bcc885419d4938d6c83df37e4efcb9a
flytecc <dlxorud12567@gmail.com>
2025-11-12 13:32:08 +0900

    11/12 수정

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,6 @@ def driver():
     # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    # options.add_experimental_option("detach", True)
     # ✅ 최신 버전 방식
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
