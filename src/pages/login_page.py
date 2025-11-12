0deb804a7bcdae8b0e3b5fd6204e17e0a06cb9dc
junseoseki <dhfkapap@naver.com>
2025-11-12 21:01:39 +0900

    Update login_page.py

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -1,7 +1,9 @@
+
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+fromm dotenv import load_dotenv
 
 
 class LoginFunction:
