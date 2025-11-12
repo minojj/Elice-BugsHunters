d79b48ae58151d90d2f48bf72a7e1dd236a3e8d6
minojj <wrnx8vzkyz@privaterelay.appleid.com>
2025-11-12 15:10:12 +0900

    error
    helper error 해결

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,12 +1,12 @@
+import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import pyautogui    #추가 import 파일업로드 창 입력 (pip install pyautogui)
-import os
 
+# pyautogui 조건부 import (CI 환경에서는 PYAUTOGUI_OFF=1 설정)
 if os.environ.get("PYAUTOGUI_OFF") != "1":
     import pyautogui
 else:
