27de78067e9e3336c14d10176906693a35eaeee2
minojj <wrnx8vzkyz@privaterelay.appleid.com>
2025-11-12 15:27:17 +0900

    hleper 수정

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,4 +1,3 @@
-import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.service import Service
@@ -6,11 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-# pyautogui 조건부 import (CI 환경에서는 PYAUTOGUI_OFF=1 설정)
-if os.environ.get("PYAUTOGUI_OFF") != "1":
-    import pyautogui
-else:
-    pyautogui = None
 
 class Utils:
     def __init__(self, driver: WebDriver):
